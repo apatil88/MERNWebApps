172e5386e636f57abeddc634072b6c7d28f8f777
Amrut Patil <apatil@emmes.com>
2018-08-28 13:44:59 -0400

    Adding mongoose and mongoDB configuration

--- SurveyCollectorWebApp/server/index.js
+++ SurveyCollectorWebApp/server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport"); //We just need to execute code in this file, nothing is being exported from this file.
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
