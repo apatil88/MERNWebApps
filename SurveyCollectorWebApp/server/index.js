e6af12d9acb761e9eb5cbb3b891991a7ad6f3a27
Amrut Patil <apatil@emmes.com>
2018-08-30 11:32:52 -0400

    Adding cookies for authentication, route handlers for sign in and logout

--- SurveyCollectorWebApp/server/index.js
+++ SurveyCollectorWebApp/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User"); //NOTE: User schema should be defined first before it is used in ./services/passport
 require("./services/passport"); //We just need to execute code in this file, nothing is being exported from this file.
@@ -7,6 +9,18 @@ require("./services/passport"); //We just need to execute code in this file, not
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+//Enable cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //Cookie should expire after 30 days
+    keys: [keys.cookieKey] //Encrypt the cookie
+  })
+);
+
+//Tell passport to use cookies to manage authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- SurveyCollectorWebApp/server/routes/authRoutes.js
+++ SurveyCollectorWebApp/server/routes/authRoutes.js
@@ -8,6 +8,17 @@ module.exports = app => {
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
 
-  //Route Handler to handler when Google directs user to /auth/google/callback with the code. This code will be sent by our server to Google to get the user profile.
+  //Route Handler to handle when Google directs user to /auth/google/callback with the code. This code will be sent by our server to Google to get the user profile.
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  //Route Handler to handle logout
+  app.get("/api/logout", (req, res) => {
+    req.logout(); //passport will unset the cookie
+    res.send(req.user);
+  });
+
+  //Route Handler to handle incoming request
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
