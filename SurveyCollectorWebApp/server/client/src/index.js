07b86ef69ad09b5f8ab7cec26da6c8d69af45574
Amrut Patil <apatil@emmes.com>
2018-09-07 10:23:09 -0400

    Adding Redux setup

--- SurveyCollectorWebApp/server/client/src/index.js
+++ SurveyCollectorWebApp/server/client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//Data Layer - Primary location for booting up Redux side of our application
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
