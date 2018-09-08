cfe98c12003ded269814ffb69b281017e8296ab8
apatil88 <amrut.patil88@hotmail.com>
2018-09-07 20:15:05 -0400

    Adding reducer for authentication status

--- SurveyCollectorWebApp/server/client/src/index.js
+++ SurveyCollectorWebApp/server/client/src/index.js
@@ -3,10 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
 import App from "./components/App";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
