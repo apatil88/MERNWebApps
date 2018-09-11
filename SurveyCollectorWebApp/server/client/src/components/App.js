5710685e95705c22c2626d951b56786d53975734
apatil88 <amrut.patil88@hotmail.com>
2018-09-10 21:37:22 -0400

    Adding Header Class Component

--- SurveyCollectorWebApp/server/client/src/components/App.js
+++ SurveyCollectorWebApp/server/client/src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "./Header";
+
 const Header = () => <h2> Header </h2>;
 const Dashboard = () => <h2> Dashboard </h2>;
 const SurveyNew = () => <h2> SurveyNew </h2>;
