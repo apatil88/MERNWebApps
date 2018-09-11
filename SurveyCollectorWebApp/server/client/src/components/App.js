67d739ab9a531973485124de17d3a4dd52dcdcec
apatil88 <amrut.patil88@hotmail.com>
2018-09-10 20:41:35 -0400

    Setting up routes using React Router and making components visible

--- SurveyCollectorWebApp/server/client/src/components/App.js
+++ SurveyCollectorWebApp/server/client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2> Header </h2>;
+const Dashboard = () => <h2> Dashboard </h2>;
+const SurveyNew = () => <h2> SurveyNew </h2>;
+const Landing = () => <h2> Landing </h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact={true} path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
