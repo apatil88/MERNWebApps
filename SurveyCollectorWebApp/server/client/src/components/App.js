473bd6ab3c63297c6eae44496d369ff3187067ef
apatil88 <amrut.patil88@hotmail.com>
2018-09-14 20:32:51 -0400

    Styling Header component, adding axios and redux thunk dependencies

--- SurveyCollectorWebApp/server/client/src/components/App.js
+++ SurveyCollectorWebApp/server/client/src/components/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./Header";
 
-const Header = () => <h2> Header </h2>;
 const Dashboard = () => <h2> Dashboard </h2>;
 const SurveyNew = () => <h2> SurveyNew </h2>;
 const Landing = () => <h2> Landing </h2>;
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
--- SurveyCollectorWebApp/server/client/src/index.js
+++ SurveyCollectorWebApp/server/client/src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
