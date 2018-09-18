d9d069908ec015e1de9af40ce98afaec714b23a7
apatil88 <amrut.patil88@hotmail.com>
2018-09-18 06:35:33 -0400

    Refactoring to Async/Await

--- SurveyCollectorWebApp/server/client/src/actions/index.js
+++ SurveyCollectorWebApp/server/client/src/actions/index.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  //redux-thunk sees that we return a function. It will automatically call it with the dispatch function.
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+//redux-thunk sees that we return a function. It will automatically call it with the dispatch function.
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
 };
