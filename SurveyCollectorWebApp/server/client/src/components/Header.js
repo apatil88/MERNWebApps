38bcdaa05a6eced2cff509f0135fb04a83745ac5
apatil88 <amrut.patil88@hotmail.com>
2018-09-18 07:27:18 -0400

    Accessing state in Header and displaying content in Header based on state

--- SurveyCollectorWebApp/server/client/src/components/Header.js
+++ SurveyCollectorWebApp/server/client/src/components/Header.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">SurveyCollector</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {
+    auth
+  };
+}
+
+export default connect()(Header);
