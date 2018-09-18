98b7872ca5113e368f0534a2dcdabb80e1f41d72
apatil88 <amrut.patil88@hotmail.com>
2018-09-18 06:51:22 -0400

    Adding return values to auth reducer

--- SurveyCollectorWebApp/server/client/src/actions/index.js
+++ SurveyCollectorWebApp/server/client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from "./types";
 //redux-thunk sees that we return a function. It will automatically call it with the dispatch function.
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- SurveyCollectorWebApp/server/client/src/reducers/authReducer.js
+++ SurveyCollectorWebApp/server/client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+export default function(state = null, action) {
+  //null is returned when the app boots up first time, indicating we have no clue whether the user is actually logged in
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+
     default:
       return state;
   }
