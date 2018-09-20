ea27830cb6ce34fc3554a1cafd6542642570fc0a
apatil88 <amrut.patil88@hotmail.com>
2018-09-19 20:48:08 -0400

    Refactoring Landing component and setting up navigation between routes based on user's authentication state

--- SurveyCollectorWebApp/server/client/src/components/Header.js
+++ SurveyCollectorWebApp/server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -24,7 +25,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">SurveyCollector</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            SurveyCollector
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
