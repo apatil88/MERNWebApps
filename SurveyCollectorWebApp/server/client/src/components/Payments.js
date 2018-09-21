c027450c8e5942fef550861efd02ebe9b2eb09da
apatil88 <amrut.patil88@hotmail.com>
2018-09-21 16:17:50 -0400

    Adding Payments component to the header

---
+++ SurveyCollectorWebApp/server/client/src/components/Payments.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Survey Collector"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
