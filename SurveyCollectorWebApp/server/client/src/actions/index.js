aaaec9fad35eb12071cfd8a71575f03b379504e1
Amrut Patil <apatil@emmes.com>
2018-09-28 15:39:39 -0400

    Adding action to POST stripe token to server

--- SurveyCollectorWebApp/server/client/src/actions/index.js
+++ SurveyCollectorWebApp/server/client/src/actions/index.js
@@ -6,3 +6,8 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
+  dispatch({ type: FETCH_USER, payload: res.data }); //Here the res.data contains the User model which contains the Google ID and stripe token
+};
