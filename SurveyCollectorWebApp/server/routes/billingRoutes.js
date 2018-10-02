829da1aa797e9d0d1e75a71cb6838f6596158d89
apatil88 <amrut.patil88@hotmail.com>
2018-10-01 21:22:27 -0400

    Adding route-specific middleware and displaying updated credits on the Header component

--- SurveyCollectorWebApp/server/routes/billingRoutes.js
+++ SurveyCollectorWebApp/server/routes/billingRoutes.js
@@ -1,12 +1,9 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ error: "You must log in!" }); //HTTP 401: Unauthorized access to resource
-    }
-
+  app.post("/api/stripe", requireLogin, async (req, res) => {
     //Get the token and reach out to Stripe API to finalize the charge
     //After finalizing the charge, update the user's number of credits in the database
     const charge = await stripe.charges.create({
