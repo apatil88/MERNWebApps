51cee82f491b679adfe7c1260736ae7495d82647
apatil88 <amrut.patil88@hotmail.com>
2018-10-04 08:13:51 -0400

    Associating a survey with a user

--- SurveyCollectorWebApp/server/models/Survey.js
+++ SurveyCollectorWebApp/server/models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
   subject: String,
   recipients: [RecipientSchema], //Array of RecipientSchema records
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" }, //This indicates the user a survey is associated with
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
