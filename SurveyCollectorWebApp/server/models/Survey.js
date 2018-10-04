6ef133cb915053c239d90d6d43146964f95c5969
apatil88 <amrut.patil88@hotmail.com>
2018-10-03 21:47:03 -0400

    Adding recipient subdocument to survey

--- SurveyCollectorWebApp/server/models/Survey.js
+++ SurveyCollectorWebApp/server/models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema], //Array of RecipientSchema records
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 }
 });
