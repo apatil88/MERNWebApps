7377cf7cb460514f81c57e1f346c33b40dbf5085
apatil88 <amrut.patil88@hotmail.com>
2018-10-06 19:09:10 -0400

    Updating style

--- SurveyCollectorWebApp/server/services/emailTemplates/surveyTemplate.js
+++ SurveyCollectorWebApp/server/services/emailTemplates/surveyTemplate.js
@@ -2,7 +2,7 @@ module.exports = survey => {
   return `
     <html>
     <body>
-        <div style="text-align; center;">
+        <div style="text-align: center;">
             <h3>I'd like your input</h3>
             <p>Please answer the following question</p>
             <p>${survey.body}</p>
