2084ab1fc391d7312b400fa1f44e1aefb3faa1b3
Amrut Patil <apatil@emmes.com>
2018-10-08 11:09:07 -0400

    Adding Dashboard component and Fixed Action Button

--- SurveyCollectorWebApp/server/client/src/components/App.js
+++ SurveyCollectorWebApp/server/client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from "../actions/index";
 
 import Header from "./Header";
 import Landing from "./Landing";
+import Dashboard from "./Dashboard";
 
-const Dashboard = () => <h2> Dashboard </h2>;
 const SurveyNew = () => <h2> SurveyNew </h2>;
 
 class App extends Component {
