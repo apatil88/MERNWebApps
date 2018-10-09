f3eea7f3f7deae31d00257bdb570aed7bd391124
apatil88 <amrut.patil88@hotmail.com>
2018-10-08 20:43:45 -0400

    Integrating Redux Form and adding surveynew component

--- SurveyCollectorWebApp/server/client/src/reducers/index.js
+++ SurveyCollectorWebApp/server/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm //Redux Form assumes all of its state lives in the 'form' key
 });
