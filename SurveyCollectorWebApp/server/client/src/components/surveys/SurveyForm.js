9ef33393c2af6cc9ffa649bdb3e76d7133a35dd1
Amrut Patil <apatil@emmes.com>
2018-10-09 12:50:48 -0400

    Adding SurveyForm component and connecting it to Redux Form

---
+++ SurveyCollectorWebApp/server/client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,13 @@
+//SurveyForm shows a form for a user to add input
+import React, { Component } from "react";
+import { reduxForm } from "redux-form";
+
+class SurveyForm extends Component {
+  render() {
+    return <div>SurveyForm</div>;
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
