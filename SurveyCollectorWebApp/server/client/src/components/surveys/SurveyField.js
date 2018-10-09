f96cd782d6c625917ce9994e4d0528d4158af7af
Amrut Patil <apatil@emmes.com>
2018-10-09 16:52:04 -0400

    Adding SurveyField to SurveyForm

---
+++ SurveyCollectorWebApp/server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+//SurveyField contains logic to render a single label and text input
+import React from "react";
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
