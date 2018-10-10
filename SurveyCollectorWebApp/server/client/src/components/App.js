daf15bee4941ead6fbe3d17292d4449058036dcc
apatil88 <amrut.patil88@hotmail.com>
2018-10-09 22:28:31 -0400

    Styling the form

--- SurveyCollectorWebApp/server/client/src/components/App.js
+++ SurveyCollectorWebApp/server/client/src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <div>
+          <div className="container">
             <Header />
             <Route exact={true} path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
