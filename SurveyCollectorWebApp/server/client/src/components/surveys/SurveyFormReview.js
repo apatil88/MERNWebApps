4c3369ba55ec9762030ec8210ade96971f6c361f
Amrut Patil <apatil@emmes.com>
2018-10-11 17:36:48 -0400

    Advancing from SurveyForm and back

--- SurveyCollectorWebApp/server/client/src/components/surveys/SurveyFormReview.js
+++ SurveyCollectorWebApp/server/client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,19 @@
+//SurveyFormReviews shows users their form inputs for review
 import React from "react";
 
-const SurveyReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <button
+        type="submit"
+        className="yellow darken-3 btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
     </div>
   );
 };
 
-export default SurveyReview;
+export default SurveyFormReview;
--- SurveyCollectorWebApp/server/client/src/components/surveys/SurveyNew.js
+++ SurveyCollectorWebApp/server/client/src/components/surveys/SurveyNew.js
@@ -10,7 +10,13 @@ class SurveyNew extends Component {
 
   renderContent() {
     if (this.state.showFormReview) {
-      return <SurveyFormReview />;
+      return (
+        <SurveyFormReview
+          onCancel={() => {
+            this.setState({ showFormReview: false });
+          }}
+        />
+      );
     }
 
     return (
