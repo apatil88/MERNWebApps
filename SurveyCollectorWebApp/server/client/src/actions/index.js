73dcf78d88d3e817259bf92cfaa5b258473642f3
Amrut Patil <apatil@emmes.com>
2018-10-16 15:57:35 -0400

    Wiring up Surveys to Redux

--- SurveyCollectorWebApp/server/client/src/actions/index.js
+++ SurveyCollectorWebApp/server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 //redux-thunk sees that we return a function. It will automatically call it with the dispatch function.
 export const fetchUser = () => async dispatch => {
@@ -16,4 +16,9 @@ export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post("/api/surveys", values);
   history.push("/surveys"); //Redirect to /surveys route after submitting the survey
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: res.data }); //Here res.data contains the list of surveys
 };
--- SurveyCollectorWebApp/server/client/src/reducers/index.js
+++ SurveyCollectorWebApp/server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm //Redux Form assumes all of its state lives in the 'form' key
+  form: reduxForm, //Redux Form assumes all of its state lives in the 'form' key
+  surveys: surveysReducer
 });
