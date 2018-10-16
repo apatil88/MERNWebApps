04bc8739f74857db533b4a6ec100767f97118fa7
Amrut Patil <apatil@emmes.com>
2018-10-16 16:19:40 -0400

    Styling list of surveys

--- SurveyCollectorWebApp/server/client/src/components/surveys/SurveyList.js
+++ SurveyCollectorWebApp/server/client/src/components/surveys/SurveyList.js
@@ -7,8 +7,28 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div key={survey._id} className="card darken-1">
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p> {survey.body}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
