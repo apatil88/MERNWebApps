4748dd9d4f367b8e0e669fa0262077e1b4524490
Amrut Patil <apatil@emmes.com>
2018-10-17 11:31:27 -0400

    Telling Heroku to install client dependencies and building client

--- SurveyCollectorWebApp/server/index.js
+++ SurveyCollectorWebApp/server/index.js
@@ -32,5 +32,16 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve production assets like main.js or main.css
+  app.use(express.static("client/build"));
+
+  //Express will serve index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
